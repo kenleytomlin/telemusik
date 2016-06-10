53b38ca8c1676dbe435bdfaea178854446211fb6
Kenley Tomlin <kenleytomlin@gmail.com>
2016-06-10 22:22:54 +0900

    Add the recommendation handler specs

---
+++ test/handlers/randomRecommendation.spec.js
@@ -0,0 +1,102 @@
+import randomRecommendation from '../../handlers/randomRecommendation'
+
+describe('handlers/randomRecommendation', () => {
+
+  function setup() {
+    const recommendationResponse = {
+      body: {
+        tracks: [
+          {
+            album: {},
+            artists: [{ id: '0987654321', name: 'Artist One' }],
+            name: 'Track One',
+            popularity: 80,
+            preview_url: 'http://preview.url',
+            track_number: 1,
+            id: '1234567890'
+          }
+        ],
+        seeds: []
+      }
+    }
+    const fiveArtistIds = [
+      '123',
+      '456',
+      '789',
+      'abc',
+      'def'
+    ]
+    const fakeSpotify = {
+      getRecommendations: expect.createSpy().andReturn(Promise.resolve())
+    }
+    const fakeBot = {
+      sendMessage: expect.createSpy().andReturn(Promise.resolve())
+    }
+    const fakeRedis = {
+      srandmember: expect.createSpy().andReturn(Promise.resolve(fiveArtistIds))
+    }
+
+    const message = {
+      message_id: 12345,
+      chat: {
+        id: 54321
+      },
+      from: {
+        id: 67890
+      }
+    }
+
+    return {
+      recommendationResponse,
+      fiveArtistIds,
+      fakeSpotify,
+      fakeBot,
+      fakeRedis,
+      message
+    }
+  }
+
+  it('returns a function', () => {
+    const { fakeSpotify, fakeRedis, fakeBot } = setup()
+    const handler = randomRecommendation(fakeSpotify,fakeBot,fakeRedis)
+    expect(handler).toBeA(Function)
+  })
+
+  it('calls redis.srandmember', () => {
+    const { fakeSpotify, fakeRedis, fakeBot, msg  } = setup()
+    const handler = randomRecommendation(fakeSpotify,fakeBot,fakeRedis)
+
+    return handler(msg).then(() => {
+      expect(fakeRedis.srandmember).toHaveBeenCalledWith(msg.from.id,5)
+    })
+  })
+
+  it('calls spotify.getRecommendations with the correct arguments', () => {
+    const { fakeSpotify, fakeRedis, fakeBot, msg, fiveArtistIds } = setup()
+    const handler = randomRecommendation(fakeSpotify,fakeBot,fakeRedis)
+
+    return handler(msg).then(() => {
+      expect(fakeSpotify.getRecommendations).toHaveBeenCalledWith({ min_energy: 0.4, seed_artists: fiveArtistIds, limit: 5, min_popularity: 50 })
+    })
+  })
+
+  it('calls fakeBot.sendMessage with the correct arguments', () => {
+    const { fakeSpotify, fakeRedis, fakeBot, msg, recommendationResponse, fiveArtistIds } = setup()
+    const handler = randomRecommendation(fakeSpotify,fakeBot,fakeRedis)
+
+    const opts = {
+      parse_mode: 'Markdown'
+    }
+
+    const md = `
+    *${recommendationResponse.body.tracks[0].name}*
+    *${recommendationResponse.body.tracks[0].album}*
+    By ${recommendationResponse.body.tracks[0].artists[0].name}
+    [Preview Link](${recommendationResponse.body.tracks[0].preview_url})
+    `
+
+    return handler(msg).then(() => {
+      expect(fakBot.sendMessage).toHaveBeenCalledWith(msg.from.id,md,opts)
+    })
+  })
+})
